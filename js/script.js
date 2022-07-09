0bac85e7608e2ddb6397ed11cd0ee06a26d4b080
Ruslan <trud55@ukr.net>
2022-07-09 23:21:16 +0300

    com4

--- js/script.js
+++ js/script.js
@@ -1,4 +1,24 @@
 "use strict";
 
-//в папке проекта  git init -- репозиторий в папке проекта
+const colfilms = +prompt("Сколько фильмов Вы просмотрели?", "Укажите цифру");
 
+const persDB = {
+  count: colfilms,
+  movies: {},
+  actors: {},
+  generes: [],
+  privat: false,
+};
+
+const a = prompt("Последний просмотренный фильм?", "Введите название фильма");
+const b = prompt("Поставьте офенку этому фильму?", "Укажите оценку от 0 до 10");
+const a2 = prompt("Последний просмотренный фильм?", "Введите название фильма");
+const b2 = prompt(
+  "Поставьте офенку этому фильму?",
+  "Укажите оценку от 0 до 10"
+);
+
+persDB.movies[a] = b;
+persDB.movies[a2] = b2;
+
+console.log(persDB);
